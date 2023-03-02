fc3bd06431f370166bdc09ee4393fc48d1766c8c
Joseph Coats <coats.joseph30@gmail.com>
2023-03-02 10:40:46 -0600

    emailjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import emailjs from '@emailjs/browser';
+import React, {useRef} from 'react'
 
 function App() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_n7g5q8m', 'template_nni8uxo', form.current, 'coB0c1m05Tf4j2jKx')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form ref={form} onSubmit={sendEmail}>
+        <br />
+        <label htmlFor="subject">Subject: </label>
+        <input type="text" name='subject' />
+        <br />
+        <br />
+        <label htmlFor="name">Name: </label>
+        <input type="text" name='name' />
+        <br />
+        <br />
+        
+        <textarea name="message" id="" placeholder='message here' cols="30" rows="10"></textarea>
+        <br />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 }
